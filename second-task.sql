f324b8bfd1f465cdacde0a052a975b4234b15e46
AgnisParis <agnettamedia@gmail.com>
2023-11-20 13:22:01 +0300

    Исправлены отсупы в файле второго задания

--- second-task.sql
+++ second-task.sql
@@ -1,9 +1,9 @@
 SELECT track,
-	   CASE
-			WHEN finished = true THEN 2
-			WHEN cancelled = true THEN -1
-			WHEN "inDelivery" = true THEN 1
-			ELSE 0 
-		END
+	CASE
+		WHEN finished = true THEN 2
+		WHEN cancelled = true THEN -1
+		WHEN "inDelivery" = true THEN 1
+		ELSE 0 
+	END
 FROM "Orders"
 ORDER BY track;
